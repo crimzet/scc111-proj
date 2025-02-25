01cd4fbb546bd56b06ff119573b8a64ea6803fc3
crimzet <alphasheep50@gmail.com>
2025-02-25 23:42:10 +0000

    better controls for player; cleaner main

--- Drinkerclass.java
+++ Drinkerclass.java
@@ -1,5 +1,24 @@
 public class Drinkerclass
 {
+    public static void runControls(GameArena arena, Player player, int speed)
+    {
+        if (arena.letterPressed('w'))
+        {
+            player.move(0, -speed);
+        }
+        if (arena.letterPressed('a'))
+        {
+            player.move(-speed, 0);
+        }
+        if (arena.letterPressed('s'))
+        {
+            player.move(0, speed);
+        }
+        if (arena.letterPressed('d'))
+        {
+            player.move(speed, 0);
+        }
+    }
     public static void main(String[] args)
     {
         GameArena arena = new GameArena(500, 300);
@@ -10,11 +29,7 @@ public class Drinkerclass
         // Main loop
         while(true)
         {
-            if (arena.letterPressed('a'))
-            {
-                player.move(-1, 0);
-                //System.out.println(player.getX());
-            }
+            runControls(arena, player, 2);
             arena.pause();
             arena.clearGameArena();
             player.addTo(arena);
